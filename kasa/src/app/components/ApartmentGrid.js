cb534925f3a001f39f95af847159cba5bc48139b
TorbosylGit <gillesplazanet@hotmail.fr>
2024-08-07 13:25:03 +0200

    Co-authored-by: Lopez <pOwn3d@users.noreply.github.com>

--- kasa/src/app/components/ApartmentGrid.js
+++ kasa/src/app/components/ApartmentGrid.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Apartment from './Apartment.js';
+import data from '../data/logement.json';
+
+// fetch('../data/logement.json').then(console.log);
+
+fetch('../data/logement.json')
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch(console.error);
 
 function ApartmentGrid() {
     return (
